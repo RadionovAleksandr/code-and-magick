83534b3c5fa09d82a537736b09947ecb1cd2608f
Антон <tat7106@gmail.com>
2018-12-08 01:04:03 +0600

    almost finished the third task

--- js/setup.js
+++ js/setup.js
@@ -13,31 +13,45 @@ var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 // Функция рандомных чисел для меняющихся предметов
-var changeElement = function (array) {
-  for (var i = 0; i < 1; i++) {
-    var randomBeginValue = 1;
-    var randomValue = Math.round(Math.random() * ((array.length - 1) - randomBeginValue));
-    return randomValue;
+var getRandomElement = function (array) {
+  var index = Math.round(Math.random() * (array.length - 1));
+  return array[index];
+};
+
+// var WIZARDS = [];
+// var WIZARDS_COUNT = 4;
+
+var createWizards = function () {
+  var wizards = [];
+  var WIZARDS_COUNT = 4;
+
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    var newWizard = {
+      name: getRandomElement(PERSON_NAME),
+      surname: getRandomElement(PERSON_SURNAME),
+      coat: getRandomElement(COAT_COLOR),
+      eyes: getRandomElement(EYES_COLOR)
+    };
+    wizards.push(newWizard);
+
+    return wizards;
   }
 };
 
-changeElement(PERSON_NAME, PERSON_SURNAME, COAT_COLOR, EYES_COLOR);
+var WIZARDS = createWizards();
 
 // Функция отрисовки волшебников в меню выбора
-var copyWizards = function () {
-  for (var i = 0; i < 4; i++) {
+var copyWizards = function (WIZARDS) {
+  for (var i = 0; i < WIZARDS.length; i++) {
     var setupSimilarItem = template.content.querySelector('.setup-similar-item').cloneNode(true);
     var space = ' ';
 
-    setupSimilarItem.querySelector('.setup-similar-label').textContent = PERSON_NAME[changeElement(PERSON_NAME)] + space + PERSON_SURNAME[changeElement(PERSON_SURNAME)];
-    setupSimilarItem.querySelector('.wizard-coat').style.fill = COAT_COLOR[changeElement(COAT_COLOR)];
-    setupSimilarItem.querySelector('.wizard-eyes').style.fill = EYES_COLOR[changeElement(EYES_COLOR)];
+    setupSimilarItem.querySelector('.setup-similar-label').textContent = WIZARDS[i].name + space + WIZARDS[i].surname;
+    setupSimilarItem.querySelector('.wizard-coat').style.fill = WIZARDS[i].coat;
+    setupSimilarItem.querySelector('.wizard-eyes').style.fill = WIZARDS[i].eyes;
 
     setupSimilarList.appendChild(setupSimilarItem);
   }
 };
 
-copyWizards();
-
-console.log(document.querySelector('.setup-similar-list'));
-console.log(document.querySelector('#similar-wizard-template'));
+copyWizards(WIZARDS);
